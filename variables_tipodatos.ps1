e0a2b606495de7be0b3665675c0cd0f83f9efb80
Ruiz Soto Victor Jose <victorjoseruizsoto4@gmail.com>
2025-02-07 23:11:48 -0700

    archivo 1 y 2

--- variables_tipodatos.ps1
+++ variables_tipodatos.ps1
@@ -1,2 +1,32 @@
 $variable = "hola"
-$variable2 = " todo esta facil"
+$variable2 = " que tal?"
+$variable3 = 100
+$variable4 = 200
+
+New-Variable -Name variable5 -Value 300
+$variable5 
+
+$variable
+$variable2
+$variable3
+$variable4
+
+$variable+$variable2
+$variable3+$variable4
+$variable3 - $variable4
+
+$$
+
+$^
+
+$?
+
+$Error
+
+get-help about_automatic_variables
+
+$ConfirmPreference
+$DebugPreference
+
+$ErrorActionPreference
+$WarningPreference
